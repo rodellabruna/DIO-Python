36115d8992481c02e85717f4b340e4b4a54ada81
Rosana Della Bruna <rosana.della.bruna@gmail.com>
2024-06-04 20:31:35 -0300

    aula sobre flake8 isort

---
+++ Manipulação/praticas_nomes.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+lista = ['banana', 'maçã']
+
+b = "jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj"
+
+a = "jaja"
+
+v = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+
